7dad59f11d9a68241e06f773b213419be8dc7647
Roopsagar K <roopsagarudayar9854@gamil.com>
2023-11-21 18:00:24 -0800

    responsiveness update

--- index.js
+++ index.js
@@ -4,27 +4,30 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const API_URL = "https://api.jikan.moe/v4"
+const API_URL = "https://api.jikan.moe/v4";
+let recommendationResult = "";
+let searchResult = "";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (req, res) => {
     const response = await axios.get(API_URL + "/anime/1/recommendations");
-    const result = response.data;
-    result.data.length = 50;
-    res.render("index.ejs", { animes: result.data, req: "root" });
+    recommendationResult = response.data.data;
+    recommendationResult.length = 50;
+    res.render("index.ejs", { animes: recommendationResult, req: "root" });
 });
 
 app.post("/search", async (req, res) => {
     const searchValue = req.body.query;
     const response = await axios.get(API_URL + `/anime?q=${searchValue}`);
-    const result = response.data;
-    res.render("searchPage.ejs", { animes: result.data, req: "search" });
+    searchResult = response.data.data;
+    res.render("searchPage.ejs", { animes: searchResult, req: "search" });
 });
 
 app.get("/animes/:title", (req, res) => {
     console.log(req.params);
+    res.render("cardPage.ejs");
 });
 
 app.listen(port, () => {
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,8 @@
 const searchItem = document.querySelector("#search");
 const form = document.querySelector("#searchForm");
 const exploreButton = document.querySelector(".cta-button");
+const card = document.querySelector(".card");
+const goto = document.querySelector(".goto");
 
 exploreButton.addEventListener('click', function(event) {
     event.preventDefault();
@@ -11,3 +13,7 @@ const kebabCase = string => string
     .replace(/([a-z])([A-Z])/g, "$1-$2")
     .replace(/[\s_]+/g, '-')
     .toLowerCase();
+
+card.addEventListener("click", function(){
+    goto.click();
+});
